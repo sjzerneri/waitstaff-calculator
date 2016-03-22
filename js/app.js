82cea1adcb470d04b5aa0002e0b6daf481aa45f9
Stevey Z <sjzerneri@gmail.com>
2016-03-22 17:27:53 -0400

    condensing to one controller, adding meal counter

--- js/app.js
+++ js/app.js
@@ -1,93 +1,28 @@
 var app = angular.module('tipCalc', ['ngAnimate'])
 
-app.constant('VERSION', "1")
-app.run(function (VERSION, $rootScope) {
-    $rootScope.version = VERSION;
 
-    var initialContent = {
-        total: 0,
-        meals: 0,
-        average: 0
-    };
+.controller('waitstaffController', function ($scope, $rootScope) {
 
-    var init = function () {
-        $rootScope.earnings = angular.copy(initialContent);
-    };
-
-    init();
-})
-
-.controller('formCtrl', function ($scope, $rootScope) {
+    var mealcount = 0;
 
     $scope.submit = function () {
         $scope.submitted = true;
         if ($scope.mealDetailForm.$valid) {
-            var subtotal, tip;
+            var subtotal, tip, total;
 
             subtotal = $scope.input.mealPrice + $scope.input.mealPrice * $scope.input.taxRate / 100;
             tip = subtotal * $scope.input.tipPercentage / 100
+            total = subtotal + tip
+            mealcount += 1;
 
-            data = {
+            $scope.data = {
                 subtotal: subtotal,
-                tip: tip
+                tip: tip,
+                total: total,
+                mealcount: mealcount
             }
-
-            console.log(
-                data.subtotal,
-                data.tip);
-
-            $rootScope.$broadcast('displayContent', data);
-
-            init();
+            console.log($scope.data.mealcount);
         }
     };
 
-    var init = function () {
-        $scope.submitted = false;
-        $scope.input = {
-            "mealPrice": "",
-            "tipPercentage": "",
-            "taxRate": ""
-        }
-    };
-
-    $scope.cancel = function () {
-        $scope.mealPrice = "";
-        $scope.taxRate = "";
-        $scope.tipPercentage = "";
-    };
-
-    init();
-})
-
-.controller('displayCtrl', function ($scope, $rootScope) {
-    var initialContent = {
-        subtotal: 0,
-        tip: 0,
-        total: 0
-    };
-
-    var init = function () {
-        $scope.data = angular.copy(initialContent);
-    };
-
-    $scope.$on('displayContent', function (event, data) {
-        $scope.data.subtotal = data.subtotal;
-        $scope.data.tip = data.tip;
-        $scope.data.total = data.subtotal + data.tip;
-        $rootScope.earnings.meals += 1;
-        $rootScope.earnings.total += data.tip;
-        $rootScope.earnings.average = $rootScope.earnings.total / $rootScope.earnings.meals;
-    });
-
-    init();
-
-
-})
-
-.controller('reset', function ($scope, $rootScope) {
-    $scope.reloadPage = function () {
-        console.log("reload");
-        window.location.reload();
-    }
 });
