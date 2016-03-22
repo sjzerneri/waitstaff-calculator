9ef1a0ee08a82a31ae2ce546c7869c7b7f4bead5
Stevey Z <sjzerneri@gmail.com>
2016-03-22 17:34:33 -0400

    consolidating code and adding in all functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ var app = angular.module('tipCalc', ['ngAnimate'])
 
 .controller('waitstaffController', function ($scope, $rootScope) {
 
-    var mealcount = 0;
+    var mealCount, tipTotal, avgTipPerMeal;
+
+    mealCount = 0
+    tipTotal = 0
+    avgTipPerMeal = 0;
 
     $scope.submit = function () {
         $scope.submitted = true;
@@ -13,13 +17,17 @@ var app = angular.module('tipCalc', ['ngAnimate'])
             subtotal = $scope.input.mealPrice + $scope.input.mealPrice * $scope.input.taxRate / 100;
             tip = subtotal * $scope.input.tipPercentage / 100
             total = subtotal + tip
-            mealcount += 1;
+            mealCount += 1
+            tipTotal += tip
+            avgTipPerMeal = tipTotal / mealCount;
 
             $scope.data = {
                 subtotal: subtotal,
                 tip: tip,
                 total: total,
-                mealcount: mealcount
+                mealCount: mealCount,
+                tipTotal: tipTotal,
+                avgTipPerMeal: avgTipPerMeal
             }
             console.log($scope.data.mealcount);
         }
